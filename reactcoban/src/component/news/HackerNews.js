d82b126ba78efae963cf6c4c92c015c1c38a7007
Spider Man <tranvandathtml0503@gmail.com>
2022-09-15 17:12:39 +0700

    hackerNews Loading

--- reactcoban/src/component/news/HackerNews.js
+++ reactcoban/src/component/news/HackerNews.js
@@ -8,20 +8,24 @@ const HackerNews = () => {
   //hook
   const [hits, setHits] = useState([]);
   const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(true);
   //handle
   // const handleUpDateQuery = lodash.debounce((e) => {
   //   setQuery(e.target.value);
   // }, 2000);
   const handFetchData = useRef({});
   handFetchData.current = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(
         `https://hn.algolia.com/api/v1/search?query=${query}`
       );
       console.log(response);
       setHits(response.data?.hits || []);
+      setLoading(!loading);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -38,9 +42,12 @@ const HackerNews = () => {
           setQuery(e.target.value);
         }}
       />
-
+      {loading && (
+        <div className="loading w-8 h-8 rounded-full border-4 border-red-300 border-r-0 border-l-0 animate-spin"></div>
+      )}
       <div>
-        {hits.length > 0 &&
+        {!loading &&
+          hits.length > 0 &&
           hits.map((item, index) => <h3 key={index}>{item.title}</h3>)}
       </div>
     </div>
