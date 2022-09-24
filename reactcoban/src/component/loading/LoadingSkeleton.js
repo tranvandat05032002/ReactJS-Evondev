24cd8e0288f08e724a76c49d5250a001b8c70240
Spider Man <tranvandathtml0503@gmail.com>
2022-09-24 20:50:17 +0700

    MovieApp with LoadinngSkeleton

--- reactcoban/src/component/loading/LoadingSkeleton.js
+++ reactcoban/src/component/loading/LoadingSkeleton.js
@@ -1,1 +1,16 @@
+import React from "react";
 
+const LoadingSkeleton = ({ width, height, radius }) => {
+  return (
+    <div
+      className="skeleton"
+      style={{
+        width: width || "100%",
+        height: height,
+        borderRadius: radius,
+      }}
+    ></div>
+  );
+};
+
+export default LoadingSkeleton;
