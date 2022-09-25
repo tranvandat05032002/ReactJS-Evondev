36fe3f1909d275f3f79029f1cd5003238c01edf0
Spider Man <tranvandathtml0503@gmail.com>
2022-09-25 15:22:27 +0700

    Validate with formik

--- reactcoban/src/component/Form/SignUpForm.js
+++ reactcoban/src/component/Form/SignUpForm.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 20) {
+    errors.firstName = "Must be less than 20 characters";
+  }
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be less than 20 characters";
+  }
+
+  return errors;
+};
+// console.log(validate);
 const SignUpForm = () => {
   const formik = useFormik({
     initialValues: {
       firstName: "",
+      lastName: "",
     },
+    validate,
     onSubmit: (values) => {
       console.log(values);
     },
@@ -24,8 +44,27 @@ const SignUpForm = () => {
           id="firstName"
           placeholder="Enter your first name"
           className="border border-gray-400 p-4 rounded-md"
+          value={formik.values.firstName}
           onChange={formik.handleChange}
         />
+        {formik.errors.firstName ? (
+          <div className="text-sm text-red-500">{formik.errors.firstName}</div>
+        ) : null}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="lastName">LastName</label>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Enter last name"
+          className="border border-gray-400 p-4 rounded-md"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.lastName ? (
+          <div className="text-sm text-blue-400">{formik.errors.lastName}</div>
+        ) : null}
       </div>
       <button
         className="p-4 bg-blue-500 font-normal w-full rounded-lg text-white"
